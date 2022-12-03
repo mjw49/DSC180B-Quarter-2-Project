5c900484880603efaa066a51d0dddaac1e80119f
Matthew Wilson <m7wilson.ucsd.edu>
2022-12-02 18:02:27 -0800

    error fixing 1

--- run.py
+++ run.py
@@ -4,12 +4,13 @@ import sys
 import json
 
 from src import motif_algorithm
+from motif_algorithm import run algorithm
 
 def main(targets):
     if 'test' in targets:
         with open('config/motif-algorithm-params.json') as fh:
             data_params = json.load(fh)
-        motif_algorithm(**data_params)
+        run_algorithm(**data_params)
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
