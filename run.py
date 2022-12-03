b51382fc87060ed000f5be3df95eaea0fcc43968
Matthew Wilson <m7wilson.ucsd.edu>
2022-12-02 17:58:37 -0800

    updated conglomerate in run file

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ def main(targets):
     if 'test' in targets:
         with open('config/motif-algorithm-params.json') as fh:
             data_params = json.load(fh)
-        run_algorithm(**data_params)
+        motif_algorithm(**data_params)
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
