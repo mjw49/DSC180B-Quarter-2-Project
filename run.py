f07ed45b7bc5b15325c121335226cbc075fdf089
mjw49 <m7wilson@ucsd.edu>
2023-02-09 07:15:17 -0800

    edited motif algorithm to include two functions, one for each algorithm

--- run.py
+++ run.py
@@ -7,9 +7,15 @@ from src import motif_algorithm
 
 def main(targets):
     if 'test' in targets:
-        with open('config/single-cluster-params.json') as fh:
+        with open('config/motif-algorithm-params.json') as fh:
             data_params = json.load(fh)
-        motif_algorithm.run_algorithm(**data_params)
+        motif_algorithm.run_k_clusters_algorithm(**data_params)
+
+    if 'city_single' in targets:
+	  with open('config/single-cluster-params.json') as fh:
+            data_params = json.load(fh)
+        motif_algorithm.run_single_cluster_algorithm(**data_params)
+
 
 if __name__ == '__main__':
     targets = sys.argv[1:]
--- src/motif_algorithm.py
+++ src/motif_algorithm.py
@@ -5,7 +5,7 @@ from .motif_adjacency_matrix import *
 from .k_clusters import *
 from .single_cluster import *
 
-def run_algorithm(fp, motif, k):
+def run_single_cluster_algorithm(fp, motif):
     
     edge_counts = motif_counting(fp, motif)
     motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)
@@ -13,6 +13,12 @@ def run_algorithm(fp, motif, k):
     print("The optimal cluster has " + str(optimal_cluster.shape[0]) + " nodes.")
     print(optimal_cluster)
     return optimal_cluster
-    #cluster_dict = k_clusters(motif_adjacency_matrix, k)    
-    #return cluster_dict
+
+
+def run_k_clusters_algorithm(fp, motif, k):
+    
+    edge_counts = motif_counting(fp, motif)
+    motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)
+    cluster_dict = k_clusters(motif_adjacency_matrix, k)    
+    return cluster_dict
     
