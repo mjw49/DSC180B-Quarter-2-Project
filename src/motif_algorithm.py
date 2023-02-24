8e2cb3479bc8f9c59a14833d1dc593d0c2e29071
mjw49 <m7wilson@ucsd.edu>
2023-02-24 09:39:01 -0800

    fixed sample function calls

--- src/motif_algorithm.py
+++ src/motif_algorithm.py
@@ -24,20 +24,20 @@ def run_k_clusters_algorithm(fp, motif, k):
     print(cluster_dict)    
     return cluster_dict
 
-def run_single_cluster_sampling_algorithm(fp, motif, sample_percent):
+def run_single_cluster_sampling_algorithm(fp, motif, threshold):
     
-    edge_counts = sample_motif_counting(fp, motif, sample_percent)
-    motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)*(1/sample_percent)
+    edge_counts = sample_motif_counting(fp, motif, threshold)
+    motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)
     optimal_cluster = single_cluster(motif_adjacency_matrix)
     print("The optimal cluster has " + str(optimal_cluster.shape[0]) + " nodes.")
     print(optimal_cluster)
     return optimal_cluster
 
 
-def run_k_clusters_sampling_algorithm(fp, motif, k, sample_percent):
+def run_k_clusters_sampling_algorithm(fp, motif, k, threshold):
     
     edge_counts = sample_motif_counting(fp, motif, sample_percent)
-    motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)*(1/sample_percent)
+    motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)
     cluster_dict = k_clusters(motif_adjacency_matrix, k)    
     return cluster_dict
     
